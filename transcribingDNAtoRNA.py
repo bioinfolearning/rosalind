a08da99205a2ae35389e02bd358e91c0aaadbecc
bioinfolearning <38712205+bioinfolearning@users.noreply.github.com>
2018-04-30 17:22:43 -0400

    Update transcribingDNAtoRNA.py
    simplified with helperfunctions.py

--- transcribingDNAtoRNA.py
+++ transcribingDNAtoRNA.py
@@ -3,18 +3,16 @@
 #2. Replace every T nucleotide with a U
 #3. write new RNA string into a file
 
-#setting file names
-input_file_name = "filename.txt"
-output_file_name = "output_filename.txt"
+from helperfunctions import get_string, write_to_file, make_output_filename
+
+#setting filenames
+input_filename = "filename.txt"
 
 #open file and prepare DNA string
-DNA_string_file = open(input_file_name)
-DNA_string = DNA_string_file.read().strip()
+DNA_string = get_string(input_filename)
 
 #replace every 'T' with 'U'
 RNA_string = DNA_string.replace("T","U")
 
 #open output file and write RNA string to it
-output_file = open(output_file_name, "w")
-output_file.write(RNA_string)
-output_file.close()
+write_to_file(input_filename, RNA_string)
