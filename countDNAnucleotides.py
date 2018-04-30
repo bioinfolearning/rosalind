510ff83c28028bc323e4d4b3243236670d8f07ee
bioinfolearning <38712205+bioinfolearning@users.noreply.github.com>
2018-04-30 16:02:39 -0400

    Update countDNAnucleotides.py
    I got the order of G and T wrong

--- countDNAnucleotides.py
+++ countDNAnucleotides.py
@@ -2,7 +2,7 @@
 #1. Open the file with the DNA string
 #2. Read from the file
 #3. Keep tally of nucleotides
-#4. Create a new file and write the tally in the order A C T and G
+#4. Create a new file and write the tally in the order A C G and T
 
 #setting file names
 input_file_name = "filename.txt"
@@ -13,5 +13,5 @@ DNA_string = DNA_string_file.readlines().strip() #should only be one line
 output_file = open(output_file_name)
 output_file.write(str(DNA_string.count("A")) + " " +
                   str(DNA_string.count("C")) + " " +
-                  str(DNA_string.count("T")) + " " +
-                  str(DNA_string.count("G")))
+                  str(DNA_string.count("G")) + " " +
+                  str(DNA_string.count("T")))
