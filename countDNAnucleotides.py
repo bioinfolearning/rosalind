fc08b0ce6f25e9223e7e8c8744df154a1afcb17c
bioinfolearning <38712205+bioinfolearning@users.noreply.github.com>
2018-04-30 15:59:18 -0400

    Update countDNAnucleotides.py

--- countDNAnucleotides.py
+++ countDNAnucleotides.py
@@ -4,9 +4,13 @@
 #3. Keep tally of nucleotides
 #4. Create a new file and write the tally in the order A C T and G
 
-DNA_string_file = open("filename.txt")
+#setting file names
+input_file_name = "filename.txt"
+output_file_name = "output.txt"
+
+DNA_string_file = open(input_file_name)
 DNA_string = DNA_string_file.readlines().strip() #should only be one line
-output_file = open("output.txt")
+output_file = open(output_file_name)
 output_file.write(str(DNA_string.count("A")) + " " +
                   str(DNA_string.count("C")) + " " +
                   str(DNA_string.count("T")) + " " +
