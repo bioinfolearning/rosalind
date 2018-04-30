8cfcea9ced74104985ef4bb76f047c0eb5fc702a
bioinfolearning <38712205+bioinfolearning@users.noreply.github.com>
2018-04-30 16:19:27 -0400

    Update countDNAnucleotides.py
    readlines had to be switched to read, since we know we're only expecting one line 
    closed file after use
    added comments

--- countDNAnucleotides.py
+++ countDNAnucleotides.py
@@ -8,10 +8,17 @@
 input_file_name = "filename.txt"
 output_file_name = "output.txt"
 
+#opening file and setting up DNA string for counting
 DNA_string_file = open(input_file_name)
-DNA_string = DNA_string_file.readlines().strip() #should only be one line
-output_file = open(output_file_name)
-output_file.write(str(DNA_string.count("A")) + " " +
-                  str(DNA_string.count("C")) + " " +
-                  str(DNA_string.count("G")) + " " +
-                  str(DNA_string.count("T")))
+DNA_string = DNA_string_file.read().strip() #should only be one line
+
+#make a tally of nucleotides
+nucleotide_tally = (str(DNA_string.count("A")) + " " +
+                    str(DNA_string.count("C")) + " " +
+                    str(DNA_string.count("G")) + " " +
+                    str(DNA_string.count("T")))
+
+#write tally to new file
+output_file = open(output_file_name, "w")
+output_file.write(nucleotide_tally)
+output_file.close()
